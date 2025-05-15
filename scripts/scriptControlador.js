0249e9ed16f01c74e91fdf63ccba435276ce9390
Omy7 <105902554+Omy7@users.noreply.github.com>
2025-05-14 20:33:12 -0600

    Delete scripts directory

--- scripts/scriptControlador.js
+++
@@ -1,8 +0,0 @@
-function toggleRectangulo() {
-    var rectangulo = document.querySelector('.rectangulo-oculto');
-    if (rectangulo.style.display === "none" || rectangulo.style.display === "") {
-        rectangulo.style.display = "block";
-    } else {
-        rectangulo.style.display = "none";
-    }
-}
